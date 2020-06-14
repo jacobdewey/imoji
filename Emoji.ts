f82d4689f47a9940de40052e1720a8a346725781
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-14 17:06:34 -0400

    Update pxt.json, main.blocks, main.ts, Emoji.ts

---
+++ Emoji.ts
@@ -0,0 +1,58 @@
+//% weight=100 color=#333333 icon="\uf224"
+namespace emojis{
+export interface emoji {
+emojis.j(h=(string=(Text.substr("😀","😃","😄","😁","😆","😅"))))
+
+
+
+
+
+}
+
+
+export function j(h: emoji): emoji{
+return h
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
