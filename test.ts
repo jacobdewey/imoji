8657d67fa81ee4f30a423a3b90af9abf7b9da7a5
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-15 17:09:19 -0400

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 //% weight=100 
 namespace tests{
+    //% block
 export function test(s: a.Block): void{
     s
 }
