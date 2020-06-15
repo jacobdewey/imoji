bb3f858ff2baefd70e0dff4a1859a56c31643902
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-15 16:39:36 -0400

    Update pxt.json, Emoji.ts

--- Emoji.ts
+++ Emoji.ts
@@ -1,27 +1,64 @@
-//% weight=100 color=#333333 icon="\uf224"
-namespace a{
-export declare interface emoji {
 
-
-
-
-
-
-
-
-
-
-
-   
-    }
-
+enum ButtonEnum{
+//% block="Button A"
+A,
+//% block="Button B"
+B,
+//% block="Button A+B"
+AB
 }
 
 
 
 
 
+//% weight=100 color=#333333 icon="\uf224"
+namespace a{
+// not done
+export type emoji ={
+Emoji(): string;
 
+
+
+
+}
+export type Button ={
+button(): ButtonEnum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
